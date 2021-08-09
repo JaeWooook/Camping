7f928718a7f2585ddd848191bff2c4aaabaa096e
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-08-09 13:35:18 +0900

    update background small size

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import Page404 from "./components/exception/Page404";
 import Background from "./store/img/camping_bg2.jpeg";
 
 const Container = styled.div`
-  background-image: url("https://imgsave.s3.us-east-2.amazonaws.com/camping_bg2.jpeg");
+  background-image: url("https://imgsave.s3.us-east-2.amazonaws.com/small_camping_bg.jpeg");
   width: 100vw;
   height: 100vh;
   background-attachment: fixed;
