390d5e9d4dd9cc0c802a700bd75f2403854350e8
JaeWook Jeong <br12345678@ajou.ac.kr>
2021-08-11 15:50:40 +0900

    add IconStar, Star point

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -2,11 +2,13 @@ import React from "react";
 import HeaderContainer from "./HeaderContainer";
 import Navbar from "../components/Navbar";
 import SlideImg from "../components/footer/SlideImg";
+import Stars from "../components/Stars";
 
 const MainContainer = () => {
   console.log("in Main page");
   return (
     <div>
+      {/* <Stars stars={4.5}></Stars> 디테일한 소수점까지는 반영되지 않는다.*/}
       <HeaderContainer />
       <Navbar />
       <SlideImg autoflow={2000} />
